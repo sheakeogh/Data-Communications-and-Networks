e93743c4b8a66fa499783b7113fae7bb7f7c4492
Shea Keogh <64904542+sheakeogh@users.noreply.github.com>
2021-03-08 22:36:47 +0000

    Create myClient.java

---
+++ Assignment 3/myClient.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+import java.net.*;
+import java.util.*;
+
+public class myClient { 
+    public static void main(String argv[]) throws Exception { 
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
+        Socket clientSocket = new Socket("localhost", 1000); 
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+        while(clientSocket.isConnected()) {
+        	System.out.println(inFromServer.lines());
+			    outToServer.writeBytes(inFromUser.readLine() + '\n'); 		
+        }
+		    clientSocket.close();  
+    } 
+} 
